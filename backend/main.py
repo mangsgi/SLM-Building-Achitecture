2bcb822a1f0e4a36157748b1e787db242ef881f3
YeomHyunseok <131951228+YeomHyunseok@users.noreply.github.com>
2025-08-13 00:41:04 +0900

    ADD routes

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.train_routes import router as train_router
-from routes.structure_routes import router as structure_router
-from routes.control_routes import router as control_router
+from routes.inference_routes import router as inference_router
+from routes.completed_routes import router as completed_router
 
 app = FastAPI(title="SLM Model Builder",
              description="SLM 모델 구조 생성 및 확인 API")
@@ -18,8 +18,9 @@ app.add_middleware(
 
 # 라우터 등록
 app.include_router(train_router, prefix="/api/v1")
-app.include_router(structure_router, prefix="/api/v1")
-app.include_router(control_router, prefix="/api/v1")
+app.include_router(inference_router, prefix="/api/v1")
+app.include_router(completed_router, prefix="/api/v1")
+
 
 @app.get("/")
 def root():
